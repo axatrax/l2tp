230b072f820c4ebb46f14970b89edbf3a3038b22
Ryan Whelan <4249368+rwhelan@users.noreply.github.com>
2019-02-22 23:26:57 -0500

    Initial Checkin

---
+++ handler.go
@@ -0,0 +1,38 @@
+package l2tp
+
+import (
+	"github.com/mdlayher/genetlink"
+	"github.com/mdlayher/netlink"
+)
+
+type handler struct {
+	genNetlinkSocket *genetlink.Conn
+	l2tpFamilyID     uint16
+	_initialized     bool
+}
+
+func (h *handler) initHander() (err error) {
+	h.genNetlinkSocket, err = genetlink.Dial(nil)
+	if err != nil {
+		return
+	}
+
+	family, err := h.genNetlinkSocket.GetFamily("l2tp")
+	if err != nil {
+		return
+	}
+
+	h.l2tpFamilyID = family.ID
+	h._initialized = true
+	return
+}
+
+func (h *handler) communicateWithKernel(nlmsg *genetlink.Message, flags netlink.HeaderFlags) ([]genetlink.Message, error) {
+	if !h._initialized {
+		if err := h.initHander(); err != nil {
+			return nil, err
+		}
+	}
+
+	return h.genNetlinkSocket.Execute(*nlmsg, h.l2tpFamilyID, flags)
+}
---
+++ types.go
@@ -0,0 +1,5 @@
+package l2tp
+
+type Session struct{}
+
+type Tunnel struct{}
